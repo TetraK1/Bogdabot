d50388f36c9417efb45bbdaafa0b12e7a074b2be
Tetr4 <13362609+TetraK1@users.noreply.github.com>
2020-06-20 21:52:28 +0100

    Improve discord deleted vid tracking database use

--- discordbot.py
+++ discordbot.py
@@ -27,33 +27,34 @@ class DiscordClient(discord.Client):
 
     async def handle_chatMsg(self, data):
         #on bot event 'chatMsg'
+        if self.bot.db is None: return
         if not self.del_vids_channel: return
         if data['msg'].split(' ', 1)[0] != 'deleted': return
         try:
             if data['meta']['action'] != True or data['meta']['addClass'] != 'action': return
         except KeyError: return
+        if not self.bot.userlist[data['username']].rank >= 2: return
 
         title = data['msg'].split('"', 1)[1].rsplit('"', 1)[0]
-        if self.bot.db is not None:
-            async with self.bot.db.pool.acquire() as connection:
-                async with connection.transaction():
-                    x = await connection.fetch("""select videos.type, videos.id, videos.title, video_adds.from_username
-                        from videos
-                        inner join video_adds
-                        on videos.id = video_adds.video_id and videos.type = video_adds.video_type
-                        /*where videos.title = $1*/
-                        where videos.title = $1
-                        order by video_adds.timestamp desc limit 1
-                        """, 
-                        title
-                    )
-        x = x[0]
+        async with self.bot.db.pool.acquire() as connection:
+            async with connection.transaction():
+                db_result = await connection.fetch("""select videos.video_type, videos.video_id, videos.title, video_adds.from_username
+                    from videos
+                    inner join video_adds
+                    on videos.video_id = video_adds.video_id and videos.video_type = video_adds.video_type
+                    where videos.title = $1
+                    order by video_adds.timestamp desc 
+                    limit 1
+                    """, 
+                    title
+                )
+        from_username = db_result[0]['from_username']
         embed = discord.Embed()
         embed.title = title
         embed.type = 'rich'
         embed.set_author(name='Video deleted')
         embed.color = 0xFF6666
-        embed.add_field(name='Posted by', value=x['from_username'])
+        embed.add_field(name='Posted by', value=from_username)
         embed.add_field(name='Deleted by', value=data['username'])
 
         if x['type'] == 'yt':
